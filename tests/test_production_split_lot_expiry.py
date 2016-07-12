801b7bab88fb8cd55b4e1294ea1d3d09c97fce02
Jes?s Mart?n Jim?nez <jesus@nan-tic.com>
2016-07-12 13:17:39 +0200

    Update test to version 4.0

--- tests/test_production_split_lot_expiry.py
+++ tests/test_production_split_lot_expiry.py
@@ -1,23 +1,17 @@
-#The COPYRIGHT file at the top level of this repository contains the full
-#copyright notices and license terms.
+#!/usr/bin/env python
+# The COPYRIGHT file at the top level of this repository contains the full
+# copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_depends
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class ProductionTestCase(unittest.TestCase):
-    'Test Production module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('production_split_lot_expiry')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+class TestCase(ModuleTestCase):
+    'Test module'
+    module = 'production_split_lot_expiry'
 
 
 def suite():
     suite = trytond.tests.test_tryton.suite()
-    suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
-            ProductionTestCase))
+    suite.addTests(unittest.TestLoader().loadTestsFromTestCase(TestCase))
     return suite
